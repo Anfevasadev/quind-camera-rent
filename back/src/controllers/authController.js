b0b4262e6051b1d8ba6a8ca8fd73a5b13e87b09e
Anfevasadev <anfevasa@gmail.com>
2024-12-23 18:08:42 -0500

    Remove commented-out user registration code from authController

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -2,50 +2,6 @@ import bcrypt from "bcrypt";
 import User from "../models/user.js";
 import { generateToken } from "../utils/auth.js";
 
-// export const register = async (req, res) => {
-//   const { name, email, password, role = "user" } = req.body;
-
-//   try {
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     const newUser = await User.create({
-//       name,
-//       email,
-//       password_hash: hashedPassword,
-//       role,
-//       banned_until: null,
-//       rented_camera_reference: null,
-//     });
-
-//     const token = generateToken(newUser);
-
-//     res.status(201).json({
-//       success: true,
-//       message: "Usuario creado exitosamente",
-//       data: {
-//         id: newUser.id,
-//         name: newUser.name,
-//         email: newUser.email,
-//         role: newUser.role,
-//         token,
-//       },
-//     });
-//   } catch (error) {
-//     if (error.name === "SequelizeUniqueConstraintError") {
-//       return res.status(400).json({
-//         success: false,
-//         message: "El email ya está registrado",
-//       });
-//     }
-
-//     res.status(500).json({
-//       success: false,
-//       message: "Error al crear usuario",
-//       error: error.message,
-//     });
-//   }
-// };
-
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
